23d1caf6bf512e7341e7dd1d22bcaaadd274c0f0
Bilalmhmd <bilalkkay@gmail.com>
2023-11-02 15:45:59 +0200

    Distance Measuring System

---
+++ project_WorkSpace/Ultrasonic_Project/Debug/Application/main.d
@@ -0,0 +1,9 @@
+Application/main.o Application/main.o: ../Application/main.c \
+  ../Application/../HAL/ultrasonic.h \
+  ../Application/../HAL/../MCAL/std_types.h ../Application/../HAL/lcd.h
+
+../Application/../HAL/ultrasonic.h:
+
+../Application/../HAL/../MCAL/std_types.h:
+
+../Application/../HAL/lcd.h:
